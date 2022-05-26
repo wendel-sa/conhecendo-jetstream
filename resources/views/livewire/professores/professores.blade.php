75622dbc115eb11f392e3a26a06b910647c2cf15
Wendel Amorim <wendel.amorim@aluno.ifsertao-pe.edu.br>
2022-05-26 20:16:08 -0300

    estudo do jetstream

--- resources/views/livewire/professores/professores.blade.php
+++ resources/views/livewire/professores/professores.blade.php
@@ -1,12 +1,13 @@
 <x-slot name="header">
     <h2 class="text-center">Professores</h2>
 </x-slot>
+
+
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
             @if (session()->has('message'))
-            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
-                role="alert">
+            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert">
                 <div class="flex">
                     <div>
                         <p class="text-sm">{{ session('message') }}</p>
@@ -14,8 +15,7 @@
                 </div>
             </div>
             @endif
-            <button wire:click="create()"
-                class="my-4 inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base font-bold text-white shadow-sm hover:bg-red-700">
+            <button wire:click="create()" class="my-4 inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base font-bold text-white shadow-sm hover:bg-red-700">
                 Create professores
             </button>
             @if($isModalOpen)
@@ -39,10 +39,8 @@
                         <td class="border px-4 py-2">{{ $professores->email}}</td>
                         <td class="border px-4 py-2">{{ $professores->mobile}}</td>
                         <td class="border px-4 py-2">
-                            <button wire:click="edit({{ $professores->id }})"
-                                class="flex px-4 py-2 bg-gray-500 text-gray-900 cursor-pointer">Edit</button>
-                            <button wire:click="delete({{ $professores->id }})"
-                                class="flex px-4 py-2 bg-red-100 text-gray-900 cursor-pointer">Delete</button>
+                            <button wire:click="edit({{ $professores->id }})" class="flex px-4 py-2 bg-gray-500 text-gray-900 cursor-pointer">Edit</button>
+                            <button wire:click="delete({{ $professores->id }})" class="flex px-4 py-2 bg-red-100 text-gray-900 cursor-pointer">Delete</button>
                         </td>
                     </tr>
                     @endforeach
